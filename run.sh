317a843aef587ead2156ae650670384554d47891
test <you@example.com>
2023-01-05 12:34:33 +0530

    sg

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 echo -n "Enter port no"
 read msg;
+sudo systemctl enable firewalld
 sudo firewall-cmd --zone=public --permanent --add-port=$msg/udp
 sudo firewall-cmd --reload
 sudo systemctl daemon-reload
